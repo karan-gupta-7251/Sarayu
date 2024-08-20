3e0250f1062e465f5ae270b8ee2208329824554b
karan-gupta-7251 <karangupta1612003@gmail.com>
2024-08-20 19:40:59 +0530

    added

--- Utils/expressError.js
+++ Utils/expressError.js
@@ -1,18 +1,10 @@
-// class expressError extends Error {
-//   constructor(status, message) {
-//     super();
-//     this.statusCode = status;
-//     this.message = message;
-//   }
-// }
-
-// module.exports =  expressError;
-
-class ExpressError extends Error {
+class expressError extends Error {
   constructor(status, message) {
-      super();
-      this.status = status;
-      this.message = message;
+    super();
+    this.statusCode = status;
+    this.message = message;
   }
 }
-module.exports = ExpressError;
+
+module.exports =  expressError;
+
--- schema.js
+++ schema.js
@@ -1,44 +1,23 @@
-// const joi = require("joi");
+const joi = require("joi");
 
-// module.exports.listingschema = joi.object({
-//   listing: joi
-//     .object({
-//       title: joi.string().required(),
-//       description: joi.string().required(),
-//       price: joi.number().required().min(0),
-//       location: joi.string().required(),
-//       country: joi.string().required(),
-//       image: joi.string().allow("", null),
-//     })
-//     .required(),
-// });
+module.exports.listingschema = joi.object({
+  listing: joi
+    .object({
+      title: joi.string().required(),
+      description: joi.string().required(),
+      price: joi.number().required().min(0),
+      location: joi.string().required(),
+      country: joi.string().required(),
+      image: joi.string().allow("", null),
+    })
+    .required(),
+});
 
-// module.exports.reviewSchema = joi.object({
-//   review: joi
-//     .object({
-//       rating: joi.number().required().min(1).max(5),
-//       comment: joi.string().required(),
-//     })
-//     .required(),
-// });
-const Joi = require("joi");
-
-let listingSchema = Joi.object({
-    listing: Joi.object({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        location: Joi.string().required(),
-        country: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string().allow("", null),
-    }).required(),
-})
-let reviewSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().required(),
-        comment: Joi.string().required()
-
-
-    }).required()
-})
-module.exports= {reviewSchema , listingSchema};
+module.exports.reviewSchema = joi.object({
+  review: joi
+    .object({
+      rating: joi.number().required().min(1).max(5),
+      comment: joi.string().required(),
+    })
+    .required(),
+});
