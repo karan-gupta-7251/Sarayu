fd67ff80b354762844f0e992ffa7f533b2568eea
karan-gupta-7251 <karangupta1612003@gmail.com>
2024-08-22 21:57:50 +0530

    added

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const Review = require("./review.js");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -23,6 +23,12 @@ const listingSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+listingSchema.post("findOneAndDelete", async (Listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: Listing.reviews } });
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
