51f163c846c3e5abf5308be76edfaf2430e6bda8
karan-gupta-7251 <karangupta1612003@gmail.com>
2024-09-08 18:55:32 +0530

    added

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../Utils/wrapAsync.js");
+const passport = require("passport");
+
 
 router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs")
+  res.render("users/signup.ejs");
 });
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      let newUser = new User({ username, email });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to Sarayu!");
+      res.redirect("/listings");
+    } catch (error) {
+      req.flash("error", error.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back to Sarayu!");
+    res.redirect("/listings");
+  }
+);
 
 module.exports = router;
