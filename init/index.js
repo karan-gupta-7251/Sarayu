8344628b48447576182c33859a09a5249ae7fdeb
karan-gupta-7251 <karangupta1612003@gmail.com>
2024-09-11 22:53:07 +0530

    added

--- init/index.js
+++ init/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const initdata = require("./data.js");
-const Listing = require("../models/listinng.js");
+const Listing = require("../models/listing.js");
 
 main()
   .then(() => {
@@ -12,10 +12,14 @@ main()
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/sarayu");
-};
+}
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initdata.data = initdata.data.map((obj) => ({
+    ...obj,
+    owner: "66e1cbaa4f5692f3aea1060f",
+  }));
   await Listing.insertMany(initdata.data);
   console.log("data was intialized");
 };
