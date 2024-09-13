2b19cde1a4fa26971d25393968d95f169768cb2f
karan-gupta-7251 <karangupta1612003@gmail.com>
2024-09-13 14:28:53 +0530

    added

--- middleware.js
+++ middleware.js
@@ -9,7 +9,8 @@ module.exports.isLoggedIn = (req, res, next) => {
 
 module.exports.saveRedirectUrl = (req, res, next) => {
   if (req.session.redirectUrl) {
-    res.locals.redirectUrl = res.session.redirectUrl;
+    // res.locals.redirectUrl = res.session.redirectUrl;
+    res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
 };
