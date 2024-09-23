9e392886e839f176beca1c558811ef162808bbc4
karan-gupta-7251 <karangupta1612003@gmail.com>
2024-09-23 16:48:27 +0530

    added

--- controllers/listings.js
+++ controllers/listings.js
@@ -29,14 +29,14 @@ module.exports.showListing = async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 };
-
 module.exports.createListing = async (req, res) => {
-  let response = geocodingClient
+  let response = await geocodingClient
     .forwardGeocode({
-      query: req.body.Listing.location,
+      query: req.body.listing.location,
       limit: 1,
     })
     .send();
+  
 
   let url = req.file.path;
   let filename = req.file.filename;
@@ -44,10 +44,12 @@ module.exports.createListing = async (req, res) => {
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
   newListing.geometry = response.body.features[0].geometry;
+  // console.log(response.body)
   await newListing.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 };
+
 
 module.exports.renderEditForm = async (req, res) => {
   let { id } = req.params;
