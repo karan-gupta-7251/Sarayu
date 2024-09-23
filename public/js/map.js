27d321660d97980af0c2eddfcb4887cec4758ba8
karan-gupta-7251 <karangupta1612003@gmail.com>
2024-09-23 17:29:52 +0530

    added

--- public/js/map.js
+++ public/js/map.js
@@ -3,9 +3,11 @@ const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v9",
   zoom: 9,
-  center: coordinates,
+  center: listing.geometry.coordinates,
 });
 
 const marker = new mapboxgl.Marker({color: "red"})
-  .setLngLat(coordinates)
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(new mapboxgl.Popup({ offset: 25})
+  .setHTML(`<h4>${listing.location}</h4> <p>Exact Location will be provided after booking</p>`))
   .addTo(map);
